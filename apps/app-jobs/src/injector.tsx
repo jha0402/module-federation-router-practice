becb1c7e473c36026ed29e399c65a43aa6452e7c
jha0402 <67306416+jha0402@users.noreply.github.com>
2024-10-17 17:32:16 +0900

    first commit

---
+++ apps/app-jobs/src/injector.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { createRouter } from "./router";
+
+import "./index.css";
+
+export type RouterType = "browser" | "memory";
+
+const inject = ({
+  rootElement,
+  basePath,
+  routerType,
+}: {
+  rootElement: HTMLElement;
+  basePath?: string;
+  routerType: RouterType;
+}) => {
+  const router = createRouter({
+    type: routerType,
+    basePath,
+  });
+
+  const root = createRoot(rootElement);
+  root.render(<RouterProvider router={router} />);
+
+  return () => queueMicrotask(() => root.unmount());
+};
+
+export { inject };
